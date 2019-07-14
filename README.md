e02e51bc45b6a81df2d4ba3fe6ade3873d49939e
Amers <amers>
2019-07-14 15:10:52 -0230

    Add background subtraction, Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 Image Processing Term Project
 Liam Waghorn & Amy Barrett
+
+test.py
+Note: Currently there are two methods of background subtraction.
+1) Mixture of Gaussians
+    The MOG function in opencv is used to subtract the background of the frames. This results in a video of the moving
+    vehicles in the foreground. More noisy.
+2) Weighted Average
+    The average of all frames is calculated to produce the background. The background is then subtracted from the
+    foreground to produce a video of the moving vehicles. Less noisy but less clear.
+
+video.avi
+Random traffic video found on the internet
+
+cars.xml
+Random trained vehicle classifier (not used yet)
--- test.py
+++ test.py
@@ -1,3 +1,56 @@
 import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('video.avi')
+# Trained XML classifiers describes some features of some object we want to detect
+# cars = cv2.CascadeClassifier('cars.xml')
+
+#Background extraction using Mixture of Gaussians
+bsub = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    # Read frames
+    _, frame = cap.read()
+    if frame is None:
+        break
+    # Apply background subtraction
+    fg = bsub.apply(frame)
+    # Show frames
+    cv2.imshow('Frame', frame)
+    cv2.imshow('Foreground', fg)
+    k = cv2.waitKey(30)
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+# # Background Subtraction Using Weighted Averages
+# _, frame = cap.read()
+# # Create numpy array of frames
+# avg = np.float32(frame)
+# while True:
+#     #capture frame by frame
+#     _, frame = cap.read()
+#     if frame is None:
+#         break
+#     #Get average of frames
+#     cv2.accumulateWeighted(frame, avg, 0.01)
+#     #performs three operations sequentially: scaling, taking an absolute value, conversion to an unsigned 8-bit type
+#     a = cv2.convertScaleAbs(avg)
+#     avg_frame = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
+#     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # Subtract background from foreground
+#     diff = cv2.absdiff(avg_frame, gray_frame)
+#     #Display background
+#     cv2.imshow('avg', a)
+#     cv2.imshow('diff', diff)
+#     #displays window for 20 ms
+#     k = cv2.waitKey(20)
+#     if k == 27:
+#         break
+# cv2.destroyAllWindows()
+# cap.release()
+
 
 print("allo")
